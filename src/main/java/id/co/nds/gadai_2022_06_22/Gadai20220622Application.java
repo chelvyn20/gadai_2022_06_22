ec2a6eedc8e72e925c84685295a9a40e0a19bf5d
chelvyn20 <chelvyn20@gmail.com>
2022-06-22 09:06:55 +0700

    first init

---
+++ src/main/java/id/co/nds/gadai_2022_06_22/Gadai20220622Application.java
@@ -0,0 +1,13 @@
+package id.co.nds.gadai_2022_06_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gadai20220622Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gadai20220622Application.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/nds/gadai_2022_06_22/Gadai20220622ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.nds.gadai_2022_06_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gadai20220622ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
